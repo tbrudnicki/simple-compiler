eaeb76498117e71d8f4a4b7ec34bf4ae4e697fef
tbrudnicki <t.brudnicki@gmail.com>
2020-12-08 21:06:42 +0100

    String, Numbers, Comment, Punctuation handlers added

--- lexer/lexer.py
+++ lexer/lexer.py
@@ -1,5 +1,7 @@
 from contextlib import contextmanager
 
+from lexer.tokens import *
+
 
 @contextmanager
 def safe_input_stream(input_stream):
@@ -19,9 +21,111 @@ class WhitespaceHandler:
 
     def handle(self, input_stream):
         input_stream.next()
-        while input_stream.peek().isspace():
+        while (not input_stream.eof()) and input_stream.peek().isspace():
             input_stream.next()
         return None
+
+
+class CommentHandler:
+
+    def __init__(self, comment_char='#'):
+        self.comment_char = comment_char
+
+    def handle(self, input_stream):
+        input_stream.next()
+        while (not input_stream.eof()) and input_stream.next() != '\n':
+            pass
+        return None
+
+    def can_handle(self, input_stream):
+        return input_stream.peek() == '#'
+
+
+class PunctuationHandler:
+    def __init__(self, punctuation_signs=[",", ":", "(", ")"]):
+        self.punctuation_signs = punctuation_signs
+
+    def can_handle(self, input_stream):
+        return input_stream.peek() in self.punctuation_signs
+
+    def handle(self, input_stream):
+        return Punctuation(input_stream.next())
+
+
+class StringHandler:
+
+    def __init__(self):
+        pass
+
+    def can_handle(self, input_stream):
+        return input_stream.peek() == '"'
+
+    def handle(self, input_stream):
+        input_stream.next()
+        aggregate = []
+        while not input_stream.eof():
+            next_element = input_stream.next()
+            if next_element == '"':
+                return String(''.join(aggregate))
+            aggregate.append(next_element)
+
+        raise input_stream.croak('Not enclosed string at ' + input_stream.current_position())
+
+
+'''
+1
+1.0
+-1
+-1.023
+1.0E-9
+10E6
+errors
+-1.023.
+'''
+
+
+class NumberHandling:
+    INITIAL = 'initial'
+    FRACTION = 'fraction'
+    SCIENTIFIC = 'scientific'
+
+    def __init__(self):
+        self.state = NumberHandling.INITIAL
+
+    def can_handle(self, input_stream):
+        stream_lookup = input_stream.peek()
+        return stream_lookup.isnumeric() or stream_lookup == '-'
+
+    def handle(self, input_stream):
+        aggregate = input_stream.next()
+
+        def handle_fraction():
+            while not input_stream.eof() and input_stream.peek().isnumeric():
+                aggregate.append(input_stream.next())
+            self.state = NumberHandling.FRACTION
+
+        def handle_scientific_notation():
+            while not input_stream.eof() and (input_stream.peek().isnumeric() or input_stream.peek() == '-'):
+                aggregate.append(input_stream.next())
+            self.state = NumberHandling.SCIENTIFIC
+
+        while not input_stream.eof():
+            stream_lookup = input_stream.peek()
+            if stream_lookup.isnumeric():
+                aggregate.append(input_stream.next())
+            elif stream_lookup == '.':
+                if self.state in [NumberHandling.INITIAL]:
+                    aggregate.append(input_stream.next())
+                    handle_fraction()
+                else:
+                    break
+            elif stream_lookup in ['e', 'E']:
+                if self.state in [NumberHandling.INITIAL, NumberHandling.FRACTION]:
+                    handle_scientific_notation()
+                else:
+                    break
+
+        return Number(''.join(aggregate))
 
 
 class Lexer:
