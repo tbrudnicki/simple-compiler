5d75a152cb02700e968c482a5c8c7bc0c087b29e
tbrudnicki <t.brudnicki@gmail.com>
2020-12-13 17:00:13 +0100

    unified stream class additional tests for proper cursor positioning in the stream

--- simplecomiler/io/stream.py
+++ simplecomiler/io/stream.py
@@ -1,7 +1,7 @@
 from io import StringIO
 
 
-class AbstractInputStream:
+class InputStream:
     class StreamPosition:
         def __init__(self, line, col):
             self.line = line
@@ -13,19 +13,38 @@ class AbstractInputStream:
         def __repr__(self):
             return self.__str__()
 
-    def __init__(self):
+        def __eq__(self, other):
+            if not isinstance(other, InputStream.StreamPosition):
+                return False
+            return self.line == other.line and self.col == other.col
+
+    def __init__(self, stream):
+        self.stream = stream
         self.pos = 0
         self.line = 1
         self.col = 0
 
     def current_position(self):
-        return AbstractInputStream.StreamPosition(self.line, self.col)
+        return InputStream.StreamPosition(self.line, self.col)
 
     def peek(self):
-        raise Exception('Invoking abstract method')
+        current_position = self.stream.tell()
+        c = self._read_single_character()
+        self.stream.seek(current_position)
+        return c
 
     def next(self):
-        raise Exception('Invoking abstract method')
+        current = self._read_single_character()
+        self.pos += 1
+        if current == '\n':
+            self.line += 1
+            self.col = 0
+        else:
+            self.col += 1
+        return current
+
+    def _read_single_character(self):
+        return self.stream.read(1)
 
     def eof(self):
         return self.peek() == ''
@@ -35,45 +54,8 @@ class AbstractInputStream:
 
 
 def file_is(file_descriptor):
-    class FileInputStream(AbstractInputStream):
-
-        def __init__(self, stream):
-            self.fd = stream
-
-        def peek(self):
-            current_position = self.fd.tell()
-            c = self._read_single_character()
-            self.fd.seek(current_position)
-            return c
-
-        def next(self):
-            current = self._read_single_character()
-            self.pos += 1
-            if current == '\n':
-                self.line += 1
-                self.col = 0
-            else:
-                self.col += 1
-            return current
-
-        def _read_single_character(self):
-            return self.fd.read(1).decode(self.stream_encoding)
-
-    return FileInputStream(file_descriptor)
+    return InputStream(file_descriptor)
 
 
 def string_is(input_string):
-    class StringInputStream(AbstractInputStream):
-        def __init__(self, string_stream):
-            self.text_stream = string_stream
-
-        def peek(self):
-            current_position = self.text_stream.tell()
-            result = self.text_stream.read(1)
-            self.text_stream.seek(current_position)
-            return result
-
-        def next(self):
-            return self.text_stream.read(1)
-
-    return StringInputStream(StringIO(input_string))
+    return InputStream(StringIO(input_string))
--- tests/stream/string_stream.py
+++ tests/stream/string_stream.py
@@ -4,7 +4,7 @@ from simplecomiler.io.stream import string_is
 
 
 class StringStreamTestCase(unittest.TestCase):
-    def test_retrieve_same_string_as_input(self):
+    def test_consume_all_stream_gives_same_string(self):
         input_sting = 'some string'
         input_stream = string_is(input_sting)
         aggregate = []
@@ -13,7 +13,7 @@ class StringStreamTestCase(unittest.TestCase):
 
         self.assertEqual(input_sting, ''.join(aggregate), '')
 
-    def test_peek_retrieves_proper_values(self):
+    def test_peek_returns_same_values_as_next(self):
         input_sting = 'some string'
         input_stream = string_is(input_sting)
         aggregate = []
@@ -22,15 +22,24 @@ class StringStreamTestCase(unittest.TestCase):
             next_result = input_stream.next()
             self.assertEqual(peek_result, next_result, 'peek should return same values as next')
 
-    def test_peek_doesnt_change_stream_position(self):
+    def test_peek_does_not_change_stream_position(self):
         input_sting = 'some string'
         input_stream = string_is(input_sting)
 
+        position_1 = input_stream.current_position()
+
         peek1_1 = input_stream.peek()
         peek1_2 = input_stream.peek()
-        next_1 = input_stream.next()
 
+        self.assertEqual(position_1, input_stream.current_position(), 'peek should not change position in the stream')
         self.assertEqual(peek1_1, peek1_2, 'peeks from same position in the stream should return same value')
+
+        next_1 = input_stream.next()
+        after_next_position = input_stream.current_position()
+        self.assertNotEqual(position_1, after_next_position, 'next should change position in the stream')
+        self.assertEqual(position_1.line, after_next_position.line, 'line should remain the same')
+        self.assertEqual(position_1.col + 1, after_next_position.col, 'next should increase col + 1')
+
         self.assertEqual(peek1_2, next_1, 'peek should return same values as next')
 
         peek2_1 = input_stream.peek()
@@ -39,6 +48,29 @@ class StringStreamTestCase(unittest.TestCase):
 
         self.assertEqual(peek2_1, peek2_2, 'peeks from same position in the stream should return same value')
         self.assertEqual(peek2_2, next_2, 'peek should return same values as next')
+
+    def test_next_properly_updates_stream_position(self):
+        input_sting = 'some string\n \nsome'
+        input_stream = string_is(input_sting)
+
+        position_1 = input_stream.current_position()
+        self.assertEqual(position_1.col, 0)
+        self.assertEqual(position_1.line, 1)
+
+        for i in range(0, 12):
+            print(input_stream.next())
+
+        position_2 = input_stream.current_position()
+
+        self.assertEqual(0, position_2.col)
+        self.assertEqual(2, position_2.line)
+
+        while not input_stream.eof():
+            input_stream.next()
+
+        position_3 = input_stream.current_position()
+        self.assertEqual(position_3.line, 3)
+        self.assertEqual(position_3.col, 4)
 
 
 if __name__ == '__main__':
