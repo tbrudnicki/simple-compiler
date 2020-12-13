7fc37fdce9fa966ef938999a8eaf5bd764c90d15
tbrudnicki <t.brudnicki@gmail.com>
2020-12-13 13:42:48 +0100

    initial for file stream testing

--- tests/stream/file_steam.py
+++ tests/stream/file_steam.py
@@ -1,7 +1,12 @@
-from unittest import TestCase
+import unittest
+from pathlib import Path
 
 
-class FileStreamTestCase(TestCase):
-    pass
+class FileStreamTestCase(unittest.TestCase):
+
+    def test_simple(self):
+        print(Path(__file__).parent.parent.parent.joinpath('data').joinpath('test'))
 
 
+if __name__ == '__main__':
+    unittest.main()
